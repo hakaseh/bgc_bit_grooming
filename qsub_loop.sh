c829e45e8d6c354b8703cf904d6c1bbb3256ff5b
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2022-03-24 21:11:46 +1100

    bug fix

--- qsub_loop.sh
+++ qsub_loop.sh
@@ -2,8 +2,8 @@
 
 for dir_num in {882..882}
 do
-#	qsub -N sigfig${dir_num} ./reduce_sigfig.sh -- /g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf_cycle4/output${dir_num}/ocean
-	qsub -N sigfig${dir_num} ./reduce_sigfig.sh -- /scratch/v45/hh0162/access-om2/archive/01deg_jra55v140_iaf_cycle4/output$dir_num/ocean
+#	qsub -N sigfig${dir_num} -- ./reduce_sigfig.sh /g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf_cycle4/output${dir_num}/ocean
+	qsub -N sigfig${dir_num} -- ./reduce_sigfig.sh /scratch/v45/hh0162/access-om2/archive/01deg_jra55v140_iaf_cycle4/output$dir_num/ocean
 #	./reduce_sigfig.sh /g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf_cycle4/output${dir_num}/ocean
 #	./reduce_sigfig.sh /scratch/v45/hh0162/access-om2/archive/01deg_jra55v140_iaf_cycle4/output$dir_num/ocean
 done
