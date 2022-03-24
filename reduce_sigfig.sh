455545d3b0517a904370e2ad04c6987ad1327dcd
hakase hayashida <hakase.hayashida@utas.edu.au>
2022-03-24 16:51:26 +1100

    update script after testing with output732. Needs to add more variable names for ppc setting.

--- reduce_sigfig.sh
+++ reduce_sigfig.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 
 
+lev_comp=5
 num_dir=732
 path_ocean=/g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf_cycle4/output${num_dir}/ocean
 
+for path_file in ${path_ocean}/oceanbgc-[2,3]d-*.nc
+do
+	name_file="${path_file##*/}"
+	trim_front="${name_file##oceanbgc-[2,3]d-}"
+	#Trim back to get variable name
+	name_var="${trim_front%%-*}"
+	#Initialise ppc to zero to avoid potential error.
+	ppc=0
+
+	#Set the sig fig according to the variable name
+
+	#ppc=4 for adic,dic,alk,o2
+	if [ "${name_var}" == "adic" ] || [ "${name_var}" == "dic" ] || [ "${name_var}" == "alk" ] || [ "${name_var}" == "o2" ]
+	then
+		ppc=4
+	elif [ "${name_var}" == "no3" ] || [ "${name_var}" == "fe" ] || [ "${name_var}" == "phy" ] || [ "${name_var}" == "zoo" ] || [ "${name_var}" == "det" ] || [ "${name_var}" == "caco3" ]
+	then
+		ppc=3
+	elif [ "${name_var}" == "stf03" ] || [ "${name_var}" == "stf07" ] || [ "${name_var}" == "stf09" ]
+	then
+		ppc=4
+	elif [ "${name_var}" == "npp2d" ] || [ "${name_var}" == "pprod_gross_2d" ] || [ "${name_var}" == "wdet100" ]
+	then
+		ppc=3
+	fi
+	#echo $name_var $ppc
+	#echo ./output/bit_groomed_ppc_${ppc}_${name_file}
+	ncks -7 -L ${lev_comp} --baa=4 --ppc default=${ppc} ${path_file} ./bit_groomed_${name_file}
+done
